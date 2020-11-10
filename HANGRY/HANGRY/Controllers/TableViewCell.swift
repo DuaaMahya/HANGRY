85447cbd83223069d285e55554117126f4d8bfaa
DuaaMahya <temoka48@gmail.com>
2020-11-10 16:46:40 +0300

    Added through iMac
    ingredients list added
    - tableview
    - entry view

---
+++ HANGRY/HANGRY/Controllers/TableViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  TableViewCell.swift
+//  toDoList
+//
+//  Created by Dua Almahyani on 03/11/2020.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+        
+    lazy var cellContainerView: UIView = {
+        let view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width, height: 70))
+        view.backgroundColor = UIColor(named: "lightPurple")
+        view.layer.cornerRadius = 9.0
+        
+        view.addSubview(ingredientImage)
+        ingredientImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        ingredientImage.anchor(top: view.topAnchor, left: view.leftAnchor,
+                               paddingTop: 12, paddingLeft: 5,
+                               width: 30, height: 30)
+        ingredientImage.layer.cornerRadius = 30/2
+        
+        view.addSubview(ingredientNameLabel)
+        ingredientNameLabel.anchor(top: view.topAnchor, left: ingredientImage.rightAnchor, paddingTop: 12, paddingLeft: 10)
+        
+        view.addSubview(remainingTimeLabel)
+        remainingTimeLabel.anchor(top: ingredientNameLabel.bottomAnchor, left: ingredientImage.rightAnchor,
+                                  paddingTop: 5, paddingLeft: 10)
+        
+        return view
+    }()
+    
+    let ingredientImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "pickImage")
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.borderWidth = 3
+        image.layer.borderColor = UIColor.white.cgColor
+        return image
+    }()
+ 
+    
+    let ingredientNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "label"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let remainingTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "due in"
+        label.font = UIFont.systemFont(ofSize: 10)
+        return label
+    }()
+    
+    var getImage = UIImage()
+    var getIngredientName = String()
+    var getRemainingTime = String()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        ingredientImage.image = getImage
+        ingredientNameLabel.text = getIngredientName
+        remainingTimeLabel.text = getRemainingTime
+        
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        contentView.backgroundColor = UIColor(named: "BGColor")
+
+        addSubview(cellContainerView)
+        cellContainerView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 20, paddingBottom: 5, paddingRight: 20)
+        
+    }
+    
+
+}
